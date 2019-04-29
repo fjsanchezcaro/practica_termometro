6596de1f9c1efeccab20eb5767f9705a0be876e7
Francisco José Sánchez-Caro Abril <fjsanchezcaro@gmail.com>
2019-04-29 17:37:12 +0200

    Cambios en la lógica del programa

--- obj_termometro.py
+++ obj_termometro.py
@@ -4,18 +4,20 @@ class Termometro():
         self.unidad = unidad
         
     def conversor(self):
-        temperatura = int(input("Introduzca la temperatura "))
-        unidad = str(input("Introduzca la escala de temperatura a la que quiere convertir C/F "))
+        temperatura = int(input("Introduzca la temperatura en grados "))
+        unidad = str(input("Introduzca la escala de temperatura"\
+                           "a la que quiere convertir (C/F) "))
         unidad = unidad.upper()
         
         if unidad == 'C':
-            temperatura = (temperatura * 9 / 5) + 32
-        elif unidad == 'F':
             temperatura = (temperatura - 32) * 5 / 9
+            unidad = 'Celsius'
+        elif unidad == 'F':
+            temperatura = (temperatura * 9 / 5) + 32
+            unidad = 'Fahrenheit'
         else:
             print("La escala introducida no corresponde a ninguna conocida")
             
-        return "La temperatura correspondiente a {} es {:.2f}".format(unidad, temperatura)
+        return "La temperatura correspondiente es {:.2f}º {}".format(temperatura, unidad)
     
-centigrado = Termometro(0, '')
-farenheit = Termometro(0, '')
+escalaGrados = Termometro(0, '')
